5f28da21c6b34e84aa08361c589c7be5303f2b9e
SAgit101 <Saadproject49@gmail.com>
2025-12-25 06:14:31 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_blob" "delete_1stfile" {
   name                   = "1stfile.txt"
-  storage_account_name   = "YOUR_STORAGE_ACCOUNT_NAME"
-  storage_container_name = "1stblob"
+  storage_account_name   = "1stblob"
+  storage_container_name = "1stcontainer"
   type                   = "Block"
 
   lifecycle {
