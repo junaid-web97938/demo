08ee99e69af236099784e21f616435a6fa4145ed
junaid-web97938 <junaid-web97@outlook.com>
2026-01-03 22:02:43 -0600

    new branch

--- providers.tf
+++ providers.tf
@@ -10,8 +10,9 @@ terraform {
 }
 
 provider "azuread" {
-variable "tenant_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-
+  tenant_id     = var.tenant_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
 }
+
+
