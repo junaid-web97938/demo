2925e93c891e7fd7597fe1648b588d3c8c51e276
junaid-web97938 <junaid-web97@outlook.com>
2026-01-07 01:01:01 -0600

    Testnew

---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "tenant_id" {ebef575a-d8a6-4c39-aa06-5df29f4abbb1}
+variable "client_id" {e5ebda94-84c4-4558-81dc-fd2c0e86a6b5}
+variable "subscription_id" {977ea651-bad0-47a1-b67b-7a93416e2a20}
