45601526655ac1f8ab50e966651972a8a8cdfc73
junaid-web97938 <junaid-web97@outlook.com>
2026-01-07 07:14:53 -0600

    Class (#4)
    * new
    
    * new
    
    * new
    
    * new
    
    * new
    
    * new
    
    * new
    
    * new
    
    * new
    
    * new

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "azuread_client_config" "current" {}
 
 resource "azuread_user" "batman" {
-  user_principal_name   = "batman@$junaidweb97outlook.onmicrosoft.com"
+  user_principal_name   = "batman@\$junaidweb97outlook.onmicrosoft.com"
   display_name          = "Batman"
   mail_nickname         = "batman"
 
