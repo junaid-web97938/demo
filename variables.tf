8a8ed143ee4132c20db2ec45aba83d2edb69b545
junaid-web97938 <junaid-web97@outlook.com>
2026-01-07 07:26:06 -0600

    a

--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
-variable "tenant_id" {}
-variable "client_id" {}
-variable "subsrciption_id" {}
+variable "tenant_id" {
+  description = "Azure AD Tenant ID"
+  type        = string
+}
+
+variable "user_password" {
+  description = "Initial password for Batman"
+  type        = string
+  sensitive   = true
+}
