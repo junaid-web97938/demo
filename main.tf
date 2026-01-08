214ff309e31ed21a645cc8026bda333f7e244682
junaid-web97938 <junaid-web97@outlook.com>
2026-01-07 23:58:40 -0600

    mnt

--- main.tf
+++
@@ -1,9 +0,0 @@
-variable "domain" {
-  default = "junaidweb97outlook.onmicrosoft.com"
-}
-
-resource "azuread_user" "batman" {
-  user_principal_name = "batman@${var.domain}"
-  display_name        = "Batman"
-  password            = "SuperSecret123!"
-}
