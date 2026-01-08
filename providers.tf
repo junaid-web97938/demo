fcd7da6cf67c918f2f7a11b411f7651b4bde6bea
junaid-web97938 <junaid-web97@outlook.com>
2026-01-07 23:13:40 -0600

    Refactor Terraform providers configuration

--- providers.tf
+++ providers.tf
@@ -1,19 +1,5 @@
-terraform {
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.47"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-  }
+provider "azurerm" {
+  features {}
 }
 
-provider "azuread" {
-  tenant_id = data.azurerm_client_config.current.tenant_id
-}
-
-data "azurerm_client_config" "current" {}
+provider "azuread" {}
