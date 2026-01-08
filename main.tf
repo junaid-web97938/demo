ab0450201eb4d97840ce2ed5d548a8a28dc3070a
junaid-web97938 <junaid-web97@outlook.com>
2026-01-07 23:31:45 -0600

    Refactor azuread_user resource password configuration

--- main.tf
+++ main.tf
@@ -1,11 +1,30 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.7"  # stable version that works with flat password syntax
+    }
+  }
+}
+
+provider "azuread" {}
+
 resource "azuread_user" "thor" {
   user_principal_name = "thor@junaidweb97outlook.onmicrosoft.com"
   display_name        = "Thor"
   mail_nickname       = "thor"
-  account_enabled     = true
 
-  password_profile {
-    password                           = "P@ssw0rd-Thor-2026!"
-    force_change_password_next_sign_in = false
-  }
+  password              = "P@ssw0rd-Thor-2026!"
+  force_password_change = false
+  account_enabled       = true
+}
+
+output "thor_user_id" {
+  value = azuread_user.thor.id
+}
+
+output "thor_upn" {
+  value = azuread_user.thor.user_principal_name
 }
