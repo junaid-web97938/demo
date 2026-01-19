5643c84cc02f27bd8157198a128682c2d6d44005
sagit101 <saadproject49@gmail.com>
2026-01-18 19:36:06 -0600

    cmnt

--- all modules/usermodule/main.tf
+++ all modules/usermodule/main.tf
@@ -2,9 +2,10 @@ resource "azuread_user" "ironman" {
   user_principal_name = "ironman@${var.tenant_domain}"
   display_name        = "Iron Man"
   mail_nickname       = "ironman"
+  account_enabled     = true
 
-  password = var.ironman_password
-
-  force_password_change = false
-  account_enabled       = true
+  password_profile {
+    password                           = var.ironman_password
+    force_change_password_next_sign_in = false
+  }
 }
