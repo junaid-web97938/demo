7537a63225fb1cfecc00dbd3367a63e83178eab6
sagit101 <saadproject49@gmail.com>
2026-01-19 01:21:04 -0600

    mnt

--- all modules/spmodule multiple/backend.tf
+++ all modules/spmodule multiple/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "1resourcegroup"
     storage_account_name = "2ndstorageacct"
     container_name       = "tfstate"
-    key                  = "entra-users.tfstate"
+    key                  = "sp.tfstate"
   }
 }
